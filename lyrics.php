adf466f2da97792298407c531b136f4fb96ed12c
MotolRochelle <motolrochelle5@gmail.com>
2022-05-24 10:34:26 +0800

    Heatwaves Page Added

--- lyrics.php
+++ lyrics.php
@@ -141,6 +141,12 @@
                             Lyrics</a>
                     </div>
                 </div>
+                <div class="col-md-6">
+                    <div class="container1 rounded">Glass Animals - Heat Waves
+                        <a class="btn btn-primary btn-sm float-end" href="songs\heatwaves.php" role="button">See
+                            Lyrics</a>
+                    </div>
+                </div>
                 
             </div><br><br>
         </div>
